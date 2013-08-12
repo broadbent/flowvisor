7849974ab4ddaefabf0a1550b7d783e9750ddf5b
Matthew Broadbent <matt@matthewbroadbent.net>
2013-08-12 16:07:46 +0100

    Added slice visualisation.
    Now possible to visualise any slice. Use -s flag to specify slice name.
    Default is underlying 'fvadmin' slice.

--- utilities/gui/fvgui.py
+++ utilities/gui/fvgui.py
@@ -9,61 +9,93 @@ from jsonrpc.json import loads, dumps, JSONEncodeException, JSONDecodeException
 
 class main():
     call_id = 1
-    dpid_count = 0
-    topology = {}
+    links = {}
+    topologies = {}
     url = None
-    json_file = None
     
     def __init__(self):
-        self.form_url()
-        self.build_full_topology()
-        print "Operation completed! Please open index.html to view topology."
+        _slice = self.parse_arguments()
+        self.get_links()
+        self.build_full_topology(_slice)
 
-    def build_full_topology(self):
-        if self.add_all_datapaths() and self.add_all_links():
-            self.write_d3_json()
-
-    def add_all_datapaths(self):
-        datapaths = self.do_fv_call("list-datapaths")
-        if datapaths != None:
-            d3_nodes = list()
-            for dpid in datapaths["result"]:
-                d3_nodes.append({"name": dpid, "group": 1, "id": self.dpid_count, "dpid": dpid})
-                self.dpid_count += 1
-            self.topology.update({"nodes":d3_nodes})
-            return True
+    def build_full_topology(self, _slice="fvadmin"):
+        if self.build_slices():
+            self.write_d3_json(_slice)
+            print "Topology successfully generated! Please open index.html to view topology."
         else:
-            print "Error: No DPIDs found in topology."
-            return False
+            print "Error: Could not generate initial topology"
 
-    def add_all_links(self):
-        links = self.do_fv_call("list-links")
-        if links != None:
-            d3_links = list()  
-            for link in links["result"]:
-                src = self.find_id_from_dpid(link["srcDPID"])
-                dst = self.find_id_from_dpid(link["dstDPID"])
-                if src != None and dst != None:
-                    d3_links.append({"source": src, "target": dst, "value" : 3})
+    def build_slices(self):
+        slices = self.do_fv_call("list-slices")
+        if slices != None:
+            for _slice in slices["result"]:
+                dpid_count = 0
+                slice_name = {}
+                slice_name["slice-name"] = _slice["slice-name"]
+                slice_health = self.do_fv_call("list-slice-health", slice_name)
+                if slice_health != None:
+                    d3_nodes = list()
+                    for dpid in slice_health["result"]["connected-dpids"]:
+                        d3_nodes.append({"name": dpid, "group": 1, "id": dpid_count, "dpid": dpid})
+                        dpid_count += 1
+                    self.topologies[_slice["slice-name"]] = {}
+                    self.topologies[_slice["slice-name"]].update({"nodes":d3_nodes})
+                    self.build_links_for_slice(_slice["slice-name"])
                 else:
-                    print "Note: Unknown DPID found in list of topology links. Ignoring."        
-            self.topology.update({"links":d3_links})
+                    print "Error: No slices found with the name '%s'" % _slice["slice-name"]
+                    return False 
             return True
         else:
-            print "Error: No links found in topology."
+            print "Error: No slices found."
+            return False 
+
+    # def build_datapaths(self):
+    #     datapaths = self.do_fv_call("list-datapaths")
+    #     if datapaths != None:
+    #         d3_nodes = list()
+    #         for dpid in datapaths["result"]:
+    #             d3_nodes.append({"name": dpid, "group": 1, "id": self.dpid_count, "dpid": dpid})
+    #             self.dpid_count += 1
+    #         self.topology.update({"nodes":d3_nodes})
+    #         return True
+    #     else:
+    #         print "Error: No DPIDs found in topology."
+    #         return False
+
+    def get_links(self):
+        self.links = self.do_fv_call("list-links")
+        if self.links != None:
+            return True
+        else:
+            print "Error: No links found in FlowVisor."
             return False
 
-    def find_id_from_dpid(self, dpid):
-        for node in self.topology["nodes"]:
+    def build_links_for_slice(self, _slice="fvadmin"):
+        d3_links = list()  
+        for link in self.links["result"]:
+            src = self.find_id_from_dpid(link["srcDPID"], _slice)
+            dst = self.find_id_from_dpid(link["dstDPID"], _slice)
+            if src != None and dst != None:
+                d3_links.append({"source": src, "target": dst, "value" : 3})
+        self.topologies[_slice].update({"links":d3_links})
+
+    def find_id_from_dpid(self, dpid, _slice="fvadmin"):
+        for node in self.topologies[_slice]["nodes"]:
             if node["dpid"] == dpid:
                 return node["id"]
         return None
 
-    def do_fv_call(self, method):
-        try:
-            post_data = dumps({"id":self.call_id, "method":method, "jsonrpc":"2.0"})
-        except JSONEncodeException as exception:
-            print "Error: Could not encode JSON for FlowVisor request: %s" % exception
+    def do_fv_call(self, method, _input=None):
+        if _input==None:
+            try:
+                post_data = dumps({"id":self.call_id, "method":method, "jsonrpc":"2.0"})
+            except JSONEncodeException as exception:
+                print "Error: Could not encode JSON for FlowVisor request: %s" % exception
+        else:
+            try:
+                post_data = dumps({"id":self.call_id, "method":method, "params":_input, "jsonrpc":"2.0"})
+            except JSONEncodeException as exception:
+                print "Error: Could not encode JSON for FlowVisor request: %s" % exception
         try:
             response_data = urllib.urlopen(self.url, post_data).read() 
             self.call_id += 1
@@ -75,26 +107,32 @@ class main():
         except IOError as exception:
             print "Error: Could not connect to FlowVisor instance: %s" % exception
 
-    def write_d3_json(self):
+    def write_d3_json(self, _slice="fvadmin"):
+        json_file = None
         try:
-            self.json_file = open('topology.json', 'w+')
+            json_file = open('topology.json', 'w+')
         except IOError as exception:
             print "Error: Could not open D3.js topology file: %s" % exception
         try:
-            self.json_file.write(dumps(self.topology))
+            try:
+                json_file.write(dumps(self.topologies[_slice]))
+            except KeyError as exception:
+                print "Error: Slice with name '%s' not found." % _slice
         except (IOError, JSONEncodeException) as exception:
             print "Error: Could not write topology to D3.js topology file: %s" % exception
-        if self.json_file != None:
-            self.json_file.close()
+        if json_file != None:
+            json_file.close()
 
-    def form_url(self):
+    def parse_arguments(self):
         parser = argparse.ArgumentParser(description='FlowVisor GUI: facilitating the visualisation of network slices in a running FlowVisor instance.')
         parser.add_argument('-i', '--ip', dest="ip", required=True, help='IP address of running FlowVisor instance.')
         parser.add_argument('-p', '--port', dest="port",default="8081", help='JSON-RPC port of running FlowVisor isntance.')
         parser.add_argument('-u', '--username', dest="username", default="fvadmin", help='Username of FlowVisor user.')
         parser.add_argument('-pw', '--password', dest="password", required=True, help='Password of FlowVisor user.')
+        parser.add_argument('-s', '--slice', dest="slice", default="fvadmin", help='FlowVisor slice you wish to visualise.')
         arguments = parser.parse_args()
         self.url =  "https://%s:%s@%s:%d" % (arguments.username, arguments.password, arguments.ip, int(arguments.port))
+        return arguments.slice
 
 
 if __name__ == "__main__":
